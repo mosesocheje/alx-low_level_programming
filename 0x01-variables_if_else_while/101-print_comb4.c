25ef8936f22ed03165a4cb453461803e4d627afa
mosesocheje <ochejemoses40@gmail.com>
2022-09-09 17:00:20 -0700

    print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,33 +7,24 @@
   */
 int main(void)
 {
-	int c, i, k, j;
+	int c, i, k;
 
-	for (c = 48; c <= 57; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		for (i = 48; i <= 57; i++)
+		for (i = '0'; i <= '9'; i++)
 		{
-			for (k = 48; k <= 57; k++)
+			for (k = '0'; k <= '9'; k++)
 			{
-				for (j = 48; j <= 57; j++)
+				if (c < i && i < k)
 				{
-					if (((k + j) > (c + i) &&  k >= c) || c < k)
-					{
-						putchar(c);
-						putchar(i);
-						putchar(' ');
-						putchar(k);
-						putchar(j);
+					putchar(c);
+					putchar(i);
+					putchar(k);
 
-					if (c + i + k + j == 227 && c == 57)
+					if (c != '7')
 					{
-					break;
-					}
-					else
-					{
-					putchar(',');
-					putchar(' ');
-					}
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
@@ -43,4 +34,3 @@ int main(void)
 	putchar('\n');
 
 	return (0);
-}
